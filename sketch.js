ffa853e00dbe0e2cef503978f5d6174d8de18961
sumanyucoder <96648190+sumanyucoder@users.noreply.github.com>
2021-12-25 14:33:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ var height;
 
 function preload(){
    bg =loadImage("Images/cityImage.png");
-   balloonImage1=loadAnimation("Images/HotAirBallon01.png");
-   balloonImage2=loadAnimation("Images/HotAirBallon01.png","Images/HotAirBallon01.png",
-   "Images/HotAirBallon01.png","Images/HotAirBallon02.png","Images/HotAirBallon02.png",
-   "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+   balloonImage1=loadAnimation("HotAirBallon01.png");
+   balloonImage2=loadAnimation("HotAirBallon01.png","HotAirBallon01.png",
+   "HotAirBallon01.png","HotAirBallon02.png","HotAirBallon02.png",
+   "HotAirBallon02.png","HotAirBallon03.png","HotAirBallon03.png","HotAirBallon03.png");
   }
 
 //Function to set initial environment
